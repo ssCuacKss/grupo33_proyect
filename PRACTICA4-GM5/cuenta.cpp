5d469bbd0eb83e5b2689636a29bab5b345aadde3
PalomaRODE4 <palomard3@gmail.com>
2020-12-10 01:51:59 +0100

    Clase cuenta completada

---
+++ PRACTICA4-GM5/cuenta.cpp
@@ -0,0 +1,9 @@
+#include <string>
+#include "cuenta.h"
+using namespace std;
+
+Cuenta:: Cuenta(string jobEmail, string contrasena, int permissionLevel){
+	correo=jobEmail;
+	password=contrasena;
+	permiso=permissionLevel;
+}

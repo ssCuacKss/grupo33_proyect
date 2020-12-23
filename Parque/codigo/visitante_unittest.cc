daddb11098418c0aba84b58f494c7a593c4ec21e
PalomaRODE4 <71469026+PalomaRODE4@users.noreply.github.com>
2020-12-23 23:27:06 +0100

    Update visitante_unittest.cc

--- Parque/codigo/visitante_unittest.cc
+++ Parque/codigo/visitante_unittest.cc
@@ -16,36 +16,33 @@ TEST(visitante, ConstructorParametrosDefecto) {
   EXPECT_EQ("", p.getDisable());
   EXPECT_EQ("", p.getTelefono());
    EXPECT_EQ("", p.getGrupo());
-   EXPECT_EQ("", p.getruta());
 }
 
 
 TEST(Visitante, ConstructorCopiaDefecto) {
-  visitante p("44XX", "Carlos", "Gutierrez", 911804488, "C/ Mesa 1", "papopepo@gmail.com", "20/11/2000",true, false,"rutaux");
+  visitante p("44XX", "Carlos", "Gutierrez Fernandez", 911804488, "C/ Mesa 1", "sn1990cagufe@sn.es", "20/11/2000");
   visitante q(p);
   visitante r=q;
 
   EXPECT_EQ("44XX", q.getDNI());
   EXPECT_EQ("Carlos", q.getNombre());
-  EXPECT_EQ("Gutierrez", q.getApellidos());
+  EXPECT_EQ("Gutierrez Fernandez", q.getApellidos());
   EXPECT_EQ("C/ Mesa 1", q.getDireccion());
-  EXPECT_EQ("20/11/2000", q.getBirthdate());
-  EXPECT_EQ("papopepo@gmail.com", q.getEmail());
+  EXPECT_EQ("20/11/1990", q.getBirthdate());
+  EXPECT_EQ("sn1990cagufe@sn.es", q.getEmail());
   EXPECT_EQ(911804488, q.getTelefono());
-  EXPECT_EQ(911804488, q.getDisable());
-  EXPECT_EQ(911804488, q.getGrupo());
-  EXPECT_EQ(911804488, q.getruta());
+  EXPECT_EQ(true, q.getDisable());
+  EXPECT_EQ(false, q.getGrupo());
 
   EXPECT_EQ("44XX", r.getDNI());
   EXPECT_EQ("Carlos", r.getNombre());
-  EXPECT_EQ("Gutierrez", r.getApellidos());
+  EXPECT_EQ("Gutierrez Fernandez", r.getApellidos());
   EXPECT_EQ("C/ Mesa 1", r.getDireccion());
-  EXPECT_EQ("20/11/2000", r.getBirthdate());
-  EXPECT_EQ("papopepo@gmail.com", r.getEmail());
+  EXPECT_EQ("20/11/1990", r.getBirthdate());
+  EXPECT_EQ("sn1990cagufe@sn.es", r.getEmail());
   EXPECT_EQ(911804488, r.getTelefono());
-  EXPECT_EQ(911804488, q.getDisable());
-  EXPECT_EQ(911804488, q.getGrupo());
-  EXPECT_EQ(911804488, q.getruta());
+  EXPECT_EQ(true, q.getDisable());
+  EXPECT_EQ(false, q.getGrupo());
 
 }
 
